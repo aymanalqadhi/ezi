2bcc1c862b1b40f98b3c9b00ef75360531d90a22
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-03-22 17:18:25 +0300

    Define basic API for the command system

--- include/command.h
+++ include/command.h
@@ -1,17 +1,38 @@
 #ifndef EZI_COMMAND_H
 #define EZI_COMMAND_H
 
+#include "config.h"
 #include "ds/slist.h"
 
-typedef int (*command_func)(const struct ezi_slist *);
+struct ezi_command_ctx;
+struct ezi_command;
+
+typedef int (*command_func)(const struct ezi_command_ctx *ctx,
+                            const struct ezi_slist *);
+
+typedef const struct ezi_command *(*get_command_func)(const char *);
+typedef int (*add_command_func)(const char *, const struct ezi_command *);
 
 struct ezi_command
 {
-    const char *name;
+    const char *display_name;
     const char *description;
     const char *usage;
 
     command_func func;
 };
+
+struct ezi_command_ctx
+{
+    const struct ezi_config *config;
+
+    get_command_func get_command;
+    add_command_func add_command;
+};
+
+extern struct ezi_command echo_command;
+
+void
+export_known_commands(add_command_func add_cmd);
 
 #endif /* EZI_COMMAND_H */
