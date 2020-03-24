c3c23be33f563c5afd5517f8f5cd6b0c46de8e6b
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-03-24 19:52:05 +0300

    Add paranthesis around macros parameters

--- include/util/validation.h
+++ include/util/validation.h
@@ -2,31 +2,31 @@
 #define EZI_UTIL_VALIDATION_H
 
 #define CHECK_NULL_PARAMS_1(p1)                                                \
-    if (!p1) {                                                                 \
+    if (!(p1)) {                                                               \
         errno = EZI_ERR_NULL_ARGUMENTS;                                        \
         return -1;                                                             \
     }
 
 #define CHECK_NULL_PARAMS_2(p1, p2)                                            \
-    if (!p1 || !p2) {                                                          \
+    if (!(p1) || !(p2)) {                                                      \
         errno = EZI_ERR_NULL_ARGUMENTS;                                        \
         return -1;                                                             \
     }
 
 #define CHECK_NULL_PARAMS_3(p1, p2, p3)                                        \
-    if (!p1 || !p2 || !p3) {                                                   \
+    if (!(p1) || !(p2) || !(p3)) {                                             \
         errno = EZI_ERR_NULL_ARGUMENTS;                                        \
         return -1;                                                             \
     }
 
 #define CHECK_NULL_PARAMS_4(p1, p2, p3, p4)                                    \
-    if (!p1 || !p2 || !p3 || !p4) {                                            \
+    if (!(p1) || !(p2) || !(p3) || !(p4)) {                                    \
         errno = EZI_ERR_NULL_ARGUMENTS;                                        \
         return -1;                                                             \
     }
 
 #define CHECK_NULL_PARAMS_5(p1, p2, p3, p4, p5)                                \
-    if (!p1 || !p2 || !p3 || !p4 || !p5) {                                     \
+    if (!(p1) || !(p2) || !(p3) || !(p4) || !(p5)) {                           \
         errno = EZI_ERR_NULL_ARGUMENTS;                                        \
         return -1;                                                             \
     }
