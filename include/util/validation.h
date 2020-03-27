d78068150c15e195c2840d5f3c364c79d6c15653
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-03-27 17:09:56 +0300

    Add uninitialized data checking macros

--- include/util/validation.h
+++ include/util/validation.h
@@ -1,6 +1,9 @@
 #ifndef EZI_UTIL_VALIDATION_H
 #define EZI_UTIL_VALIDATION_H
 
+#include "log/errno.h"
+#include <errno.h>
+
 #define CHECK_NULL_PARAMS_1(p1)                                                \
     if (!(p1)) {                                                               \
         errno = EZI_ERR_NULL_ARGUMENTS;                                        \
@@ -28,6 +31,36 @@
 #define CHECK_NULL_PARAMS_5(p1, p2, p3, p4, p5)                                \
     if (!(p1) || !(p2) || !(p3) || !(p4) || !(p5)) {                           \
         errno = EZI_ERR_NULL_ARGUMENTS;                                        \
+        return -1;                                                             \
+    }
+
+#define CHECK_UNINITIALIZED_DATA_1(d1)                                         \
+    if (!(d1)) {                                                               \
+        errno = EZI_ERR_UNINITIALIZED_DATA;                                    \
+        return -1;                                                             \
+    }
+
+#define CHECK_UNINITIALIZED_DATA_2(d1, d2)                                     \
+    if (!(d1) || !(d2)) {                                                      \
+        errno = EZI_ERR_UNINITIALIZED_DATA;                                    \
+        return -1;                                                             \
+    }
+
+#define CHECK_UNINITIALIZED_DATA_3(d1, d2, d3)                                 \
+    if (!(d1) || !(d2) || !(d3)) {                                             \
+        errno = EZI_ERR_UNINITIALIZED_DATA;                                    \
+        return -1;                                                             \
+    }
+
+#define CHECK_UNINITIALIZED_DATA_4(d1, d2, d3, d4)                             \
+    if (!(d1) || !(d2) || !(d3) || !(d4)) {                                    \
+        errno = EZI_ERR_UNINITIALIZED_DATA;                                    \
+        return -1;                                                             \
+    }
+
+#define CHECK_UNINITIALIZED_DATA_5(d1, d2, d3, d4, d5)                         \
+    if (!(d1) || !(d2) || !(d3) || !(d4) || !(d5)) {                           \
+        errno = EZI_ERR_UNINITIALIZED_DATA;                                    \
         return -1;                                                             \
     }
 
