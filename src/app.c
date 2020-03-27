2ddb4e3e8126362402f2480b7e5586dfda4f03fd
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-03-27 18:16:59 +0300

    Create and load software database

--- src/app.c
+++ src/app.c
@@ -2,6 +2,8 @@
 
 #include "command.h"
 #include "config.h"
+#include "data/software_database.h"
+#include "data/software_source.h"
 #include "ds/hash_table.h"
 #include "log/errno.h"
 #include "log/logger.h"
@@ -12,8 +14,9 @@
 #define COMMAND_MAX_NAME_LEN 0x20
 #define COMMANDS_HT_BUCKETS  0x10
 
-static struct ezi_config     config;
-static struct ezi_hash_table commands;
+static struct ezi_config            config;
+static struct ezi_hash_table        commands;
+static struct ezi_software_database db;
 
 static const struct ezi_command *
 get_command(const char *name);
@@ -28,6 +31,7 @@ int
 ezi_app_run(int argc, char *argv[])
 {
     static struct ezi_command_ctx command_ctx = { .config      = &config,
+                                                  .db          = &db,
                                                   .get_command = &get_command };
     const struct ezi_command *    cmd;
 
@@ -53,18 +57,50 @@ ezi_app_destroy(void)
 {
     free_ezi_config(&config);
     free_ezi_hash_table(&commands);
+    free_ezi_software_database(&db);
 }
 
 static int
 init_app(int argc, char *argv[])
 {
+    struct ezi_software_source isrc, asrc;
+    char                       fnbuf[0x100];
+
     if (ezi_config_parse_argv(&config, argc, argv) != 0 ||
         init_ezi_hash_table(&commands,
                             COMMANDS_HT_BUCKETS,
                             sizeof(char[COMMAND_MAX_NAME_LEN]),
                             sizeof(struct ezi_command)) != 0) {
-        log_perror("app", "ezi_config_parse_argv");
-        errno = EZI_ERR_INITIALIZATION_FAILED;
+        return -1;
+    }
+
+    log_debug("app", "Opening installed software table");
+    snprintf(fnbuf,
+             sizeof(fnbuf),
+             config.installed_table_fmt,
+             config.database_version);
+    if (init_ezi_software_source_fs(&isrc, fnbuf, config.database_version) !=
+        0) {
+        return -1;
+    }
+
+    log_debug("app", "Opening available software table");
+    snprintf(fnbuf,
+             sizeof(fnbuf),
+             config.available_table_fmt,
+             config.database_version);
+    if (init_ezi_software_source_fs(&asrc, fnbuf, config.database_version) !=
+        0) {
+        return -1;
+    }
+
+    log_debug("app", "Creating a database object");
+    if (init_ezi_software_database(&db, &isrc, &asrc) != 0) {
+        return -1;
+    }
+
+    log_norm("app", "Loading the software database");
+    if (ezi_software_database_load(&db) != 0) {
         return -1;
     }
 
