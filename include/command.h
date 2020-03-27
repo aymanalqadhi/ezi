a96dcf243eb08cad3977f9ea797c5d9c1f271522
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-03-27 18:15:10 +0300

    Add a pointer to the database in context and add a command to add software records

--- include/command.h
+++ include/command.h
@@ -3,6 +3,7 @@
 
 #include "config.h"
 #include "ds/slist.h"
+#include "data/software_database.h"
 
 struct ezi_command_ctx;
 struct ezi_command;
@@ -25,12 +26,14 @@ struct ezi_command
 struct ezi_command_ctx
 {
     const struct ezi_config *config;
+    struct ezi_software_database *db;
 
     get_command_func get_command;
     add_command_func add_command;
 };
 
 extern struct ezi_command echo_command;
+extern struct ezi_command add_software_command;
 
 void
 export_known_commands(add_command_func add_cmd);
