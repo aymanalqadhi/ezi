0f6a913b20235e9ad0d29ffca414fec6c55ade53
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-03-29 21:10:30 +0300

    Add usage,install commands to app commands list

--- src/command.c
+++ src/command.c
@@ -6,4 +6,6 @@ export_known_commands(add_command_func add_cmd)
     (*add_cmd)("echo", &echo_command);
     (*add_cmd)("add", &add_software_command);
     (*add_cmd)("list", &list_software_command);
+    (*add_cmd)("install", &install_software_command);
+    (*add_cmd)("usage", &show_usage_command);
 }
